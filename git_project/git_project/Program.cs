d5a514ef721b11ea1645a856d713fc432da9261d
Danilo Carrijo <danilo.carrijo@buildium.com>
2024-10-08 15:09:49 +0100

    add

--- git_project/git_project/Program.cs
+++ git_project/git_project/Program.cs
@@ -41,6 +41,11 @@ class Program
                 string j = "2";
             }
 
+            if (true)
+            {
+                string j = "2";
+            }
+
             string teste = "3";
 
             Console.WriteLine("Digite o primeiro número:");
